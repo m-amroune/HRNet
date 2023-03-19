fcc944ba987766a1766b552d65be16b426557ba4
m-amroune <amroune.moustapha@gmail.com>
2023-03-19 13:07:48 +0100

    add headColumns to table

--- src/pages/ListEmployees.js
+++ src/pages/ListEmployees.js
@@ -2,9 +2,21 @@ import React from "react";
 import { EmployeesTable } from "m-a_plugin_table";
 
 const ListEmployees = () => {
+  const headColumns = [
+    { title: "First Name", value: "firstName" },
+    { title: "Last Name", value: "lastName" },
+    { title: "Start Date", value: "startDate" },
+    { title: "Department", value: "department" },
+    { title: "Date of Birth", value: "dateOFBirth" },
+    { title: "Street", value: "street" },
+    { title: "City", value: "city" },
+    { title: "State", value: "state" },
+    { title: "Zip Code", value: "zipCode" },
+  ];
+
   return (
     <div>
-      <EmployeesTable />
+      <EmployeesTable headColumns={headColumns} />
     </div>
   );
 };
